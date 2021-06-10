a590e519ae39b351089caef96e5ee1e4f838daec
Brandon <hutchingsonb@gmail.com>
2021-06-10 14:12:59 -0400

    adding media queries to navbar to add hamburger icon when screen reaches 600px

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,34 +1,37 @@
 import React from 'react'
 import { Link } from "react-router-dom"; 
-import { AppBar, Toolbar, Typography, IconButton, Button } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, IconButton, Button, useMediaQuery, useTheme } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+import useStyles from "./style";
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
 
 function NavBar() {
   const classes = useStyles();
 
+  const theme = useTheme();
+
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
+
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-        </IconButton>
         <Typography variant="h6" className={classes.title}>
           Brandon Hutchingson
         </Typography>
-        <Button color="inherit">
+
+        {isSmall ? 
+          <>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+          </>
+         : 
+          <>
+            <Button color="inherit">
           <Link 
               to="/"
               className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
@@ -60,6 +63,10 @@ function NavBar() {
               Contact
           </Link>
         </Button>
+          </>
+         }
+
+        
       </Toolbar>
     </AppBar>
   )
