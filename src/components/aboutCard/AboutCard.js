5a950d81435413d1630c4ab5ecb7c16e272c9e85
Brandon <hutchingsonb@gmail.com>
2021-06-14 22:45:04 -0400

    added skills to about page

--- src/components/aboutCard/AboutCard.js
+++ src/components/aboutCard/AboutCard.js
@@ -5,29 +5,56 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import useStyles from './styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-
+const skillsArray = [
+  'Html',
+  'CSS',
+  'Javascript',
+  'JQuery',
+  'MongoDB',
+  'MySQL',
+  'Node.js',
+  'Express.js',
+  'Bootstrap',
+  'Material UI',
+  'Handlebars.js',
+  'React.js',
+]
 
 const SimpleCard = () => {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
-  return ( <>
-    <div className={classes.backdrop1}></div>
-    <Card className={classes.root}>
-      <CardContent className={classes.content}>
-        <Typography className={classes.text} variant="h5" component="h2" gutterBottom>
-          About Me
-        </Typography>
-        <Typography className={classes.about} variant="body2" component="p">
-        What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
-        </Typography>
-      </CardContent>
-      <CardActions className={classes.contactBtn}>
-        <Button className={classes.btn} variant="primary" size="medium">Contact Me</Button>
-      </CardActions>
-    </Card>
-  </>
+  return (
+    <div className={classes.aboutPage}>
+      <div className={classes.cardContainer}>
+        <Card className={classes.root}>
+          <CardContent className={classes.content}>
+            <Typography className={classes.text} variant="h5" component="h2" gutterBottom>
+              About Me
+          </Typography>
+            <Typography className={classes.about} variant="body2" component="p">
+              What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+          </Typography>
+          </CardContent>
+          <CardActions className={classes.contactBtn}>
+            <Button className={classes.btn} variant="primary" size="medium">Contact Me</Button>
+          </CardActions>
+        </Card>
+      </div>
+      <div className={classes.grid}>
+        <Grid className={classes.cardContainer} container spacing={2} >
+          {skillsArray.map(skill => {
+            return (
+              <Grid className={classes.cardContainer} key={skill} item xs={4}>
+                <Paper item className={`${classes.root} ${classes.skillItem}`}>{skill}</Paper>
+              </Grid>
+            )
+          })}
+        </Grid>
+      </div>
+    </div>
   );
 }
 
--- src/components/aboutCard/styles.js
+++ src/components/aboutCard/styles.js
@@ -13,10 +13,11 @@ const useStyles = makeStyles({
     borderStyle: 'solid', 
     borderColor: 'rgb(255, 255, 255, .6)',
     flexDirection: 'column',
-    width: 500,
+    width: 400,
     boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
     backdropFilter: "blur(8px)",
     background: 'linear-gradient(top left, rgb(255, 255, 255, .4), rgb(255, 255, 255, .05))',
+    marginBottom: 50
   },
   background: {
     backgroundImage: 'url("../../images/background.png")'
@@ -53,7 +54,29 @@ const useStyles = makeStyles({
   },
   pos: {
     marginBottom: 12,
-  }
+  },
+  skillItem: {
+    margin: 1,
+    width: '80px',
+    padding: 10,
+    textAlign: 'center'
+  },
+  aboutPage: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  grid: {
+    display: 'flex',
+    width: '70%'
+  },
+  cardContainer: {
+    display: 'flex',
+    justifyContent: 'center', 
+  },
+
 });
 
 export default useStyles;
