968be4a546fdb79c2dd0a80ceeb356113183ec78
Brandon <hutchingsonb@gmail.com>
2021-06-22 18:20:30 -0400

    adding hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./style.css"
 import NavBar from "./components/navBar/NavBar";
 import Projects from "./pages/projects/Projects";
